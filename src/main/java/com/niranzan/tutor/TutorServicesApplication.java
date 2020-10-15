0ef36ad9a2e282267445b99ef591ace5b86ab04e
Niranjan <niranjanmaharana95@gmail.com>
2020-10-15 17:08:44 +0530

    initial commits

---
+++ src/main/java/com/niranzan/tutor/TutorServicesApplication.java
@@ -0,0 +1,13 @@
+package com.niranzan.tutor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/niranzan/tutor/TutorServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.niranzan.tutor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
